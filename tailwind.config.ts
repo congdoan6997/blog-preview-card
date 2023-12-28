b6cdb22adb5fbdbdf0af5c52df645fcefb7f8104
PhDBui <congdoan6997@gmail.com>
2023-12-28 15:59:55 +0300

    create blog preview card

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "primary-yellow": "hsl(47, 88%, 63%)",
+      "neutral-white": "hsl(0, 0%, 100%)",
+      "neutral-black": "hsl(0, 0%, 7%)",
+      "neutral-gray": "hsl(0, 0%, 41%)",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      boxShadow: {
+        card: "7px 7px 0px 0px rgba(0, 0, 0, 0.93)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
